ab7a6546d74ed6ca8ed9c4767fb4848cfec552f7
namanrajpal16 <namanrajpal16@ufl.edu>
2016-08-27 23:12:49 -0400

    First Commit

---
+++ src/com/nmn/Main.java
@@ -0,0 +1,42 @@
+//Author Naman Rajpal, Ash Law, Mansi Thawani
+package com.nmn;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //ArrayList<ArrayList<String>> text = new ArrayList<ArrayList<String>>();
+        ArrayList<String> text = new ArrayList<String>();
+        String line = new String();
+        //System.out.println("Starting");
+        Scanner filein = null;
+        try {
+            filein = new Scanner(new File("C:\\Users\\Naman Rajpal\\Desktop\\Homeowrk1\\input.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        //adding text to main Array 'text'
+        while(filein.hasNextLine())
+        {
+            line = filein.nextLine();
+            text.add(line);
+        }
+        System.out.println(text.size());
+        //printing text, looping on i
+        int i=0;
+        do
+        {
+            System.out.println(text.get(i));
+            i++;
+        }while(text.size()!=i);
+
+    }
+}
