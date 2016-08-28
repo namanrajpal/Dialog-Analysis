1e3ae34984cdf2058267dcc12bb524f6432470e2
Ash Law <asheslaw@gmail.com>
2016-08-28 00:16:06 -0400

    Part 1 Completed

--- src/com/nmn/Main.java
+++ src/com/nmn/Main.java
@@ -31,12 +31,35 @@ public class Main {
         }
         System.out.println(text.size());
         //printing text, looping on i
-        int i=0;
+        /*int i=0;
         do
         {
             System.out.println(text.get(i));
             i++;
         }while(text.size()!=i);
-
+        */
+        //Part 1: Count number of dialogue turns of each interlocutor
+        int i=0;
+        int[] person = new int[2];
+        person[0] = 0;
+        person[1] = 0;
+        do
+        {
+            String currentDialog = text.get(i);
+            char c;
+            for(int j = 0; j < currentDialog.length(); j++){
+                c = currentDialog.charAt(j);
+                if(c == 'A'){
+                    person[0]++;
+                    break;
+                }
+                else if(c == 'B'){
+                    person[1]++;
+                    break;
+                }
+            }
+            i+=2;
+        }while(text.size()>i);
+        System.out.println("Person A: " + person[0] + "\nPerson B: " + person[1]);
     }
 }
